200a62693383526fdd64a4c108a8d2ed66a0a7fe
Bikrant <shilpakarbikrant@gmail.com>
2026-09-07 18:25:11 +0545

    feat(cors): configure cross-origin headers and finalize frontend integration readiness

--- portfolio_backend/settings.py
+++ portfolio_backend/settings.py
@@ -199,7 +199,4 @@ LOGGING = {
     },
 }
 
-CORS_ALLOWED_ORIGINS = [
-    'http://127.0.0.1:5500',
-    'http://localhost:5500',
-]
+CORS_ALLOWED_ORIGINS = env.list('CORS_ALLOWED_ORIGINS', default=[])
